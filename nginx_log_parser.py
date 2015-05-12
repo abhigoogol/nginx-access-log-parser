83263c1ec8ecf00f4219c64a75561809dbe37ac7
Abhishek  Singh Sambyal <abhishek.sambyal@gmail.com>
2015-05-12 11:58:55 +0530

    Compare IP address and store
    Compared IP address and stored unique address

--- nginx_log_parser.py
+++ nginx_log_parser.py
@@ -1,3 +1,7 @@
+__author__ = "Abhishek Singh Sambyal"
+__email__ = "abhishek.sambyal@gmail.com"
+__license__ = "GNU GENERAL PUBLIC LICENSE Version 3"
+
 import re, httpagentparser
 from datetime import datetime
 
@@ -78,27 +82,59 @@ def pageviewparameters(log):
 	#print user_ag[1]
 	
 	z=0
-	boundvar = 0
+	
 	count_list = []
-	print 'hhhhhhhhhi'
-	print view[0][3]
+	#print 'hhhhhhhhhi'
+	#print view[0][3]
+	unique_urls = []
+	#print uni_url
+	#print 'hiiiiii'
 	
-	print uni_url
-	print 'hiiiiii'
-	
-	for x in uni_url[0:1]:
+	for x in uni_url:
 		for y in view:
-			if x == y[3]:
+			if x.rstrip() == y[3].rstrip():
 				count_list.append(y)
-		size_list = len(count_list)
-		print count_list[0]
-		print count_list[1]
-		for k in 
+		size_list = len(count_list)-1
+		#pprint count_list[0]
+		#print count_list[1]
+		
+		print '------------'
+		print count_list
+		boundvar = 0
+		check = 0
+		for k in count_list:
 			if boundvar < size_list:
-				if count_list[boundvar] != :
-				pass
-	print'2222222222222'
-	print count_list[0]
+				print boundvar
+				ipfield = count_list[boundvar][0]
+				#print '4444444'
+				#print count_list[1][0]
+				ipfield_next = count_list[boundvar+1][0]
+				#print '5555555555'
+				#print count_list[2][0]
+				print '1111111'
+				print boundvar, size_list
+				print ipfield, ipfield_next
+				if ipfield != ipfield_next:
+					unique_urls.append(k)
+					if boundvar == size_list - 1:
+						check = 1
+					print 'yesssssss'
+				else:
+					print 'nooooooo'
+			boundvar = boundvar + 1
+		if check == 1:
+			print 'cheeeeeeeeeeeeekkkkkkk'
+			unique_urls.append(k)
+			check = 0
+		else:
+			continue
+		
+		print '3333333333'
+		print unique_urls
+		return
+	#print'2222222222222'
+	##print count_list[0]
+	#print count_list[0][1]
 	#print count_list[len(count_list)-2:len(count_list)]
 	#print count_list
 	#return
