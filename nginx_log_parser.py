3ac751bbc19c954edfa380e7f5cc11c7c5738a73
Abhishek  Singh Sambyal <abhishek.sambyal@gmail.com>
2015-05-12 16:42:18 +0530

    Complete Unique visits to URL
    Rough comments and prints are not removed yet.

--- nginx_log_parser.py
+++ nginx_log_parser.py
@@ -73,10 +73,11 @@ def pageviewparameters(log, N):
 	count_list = []
 	#print 'hhhhhhhhhi'
 	#print view[0][3]
-	unique_urls = []      
+	      
 	time_filter = []
 	#print uni_url
 	#print 'hiiiiii'
+	unique_urls = []
 	
 	for x in uni_url:
 		count_list = []
@@ -84,120 +85,89 @@ def pageviewparameters(log, N):
 			if x.rstrip() == y[3].rstrip():
 				count_list.append(y)
 		size_list = len(count_list)
-		#pprint count_list[0]
-		#print count_list[1]
-		
-		#print '-----count_list-------'
-		#print count_list
 		boundvar = 1
 		check = 0
 		url_count = 0
-		#for x in xrange(len(count_list)):
-		#	time_filter.append(re.findall(r'\d+/\w+/\d+:(\d+:\d+:\d+)\s', count_list[x][1]))
-		for k in count_list:
-			if boundvar < size_list:
-				print boundvar
-				print '-------------------k---------------'
-				print k
-				print '-------------------input field _ next---------------'
-				ipfield_next = count_list[boundvar][0]
-				print ipfield_next
-				#print '4444444'
-				#ipfield_next = count_list[boundvar+1][0]
-				#print '5555555555'
-				#print boundvar, size_list
-				#print ipfield, ipfield_next
-				print"-----------count_list------------"
-				print count_list
-				#Checking if IP address of two consecutive positions
-				if url_count == 0:
-					unique_urls.append(k)
-				print '------------unique_urls----------------'
-				print unique_urls[url_count][0]
+		first_count = 0
+		for k in count_list[1:]:
+			print '-------------------k---------------'
+			print k
+			print '-------------------input field _ next---------------'
+			ipfield_next = k[0]
+			print ipfield_next
+			print"-----------count_list------------"
+			print count_list
+			print '------------url_count----------------'
+			print url_count
+			print '------------first_count----------------'
+			print first_count
+			#Checking if IP address of two consecutive positions
+			if first_count == 0:
+				unique_urls.append(count_list[0])
+				first_count = 1
+			print '------------first_count----------------'
+			print first_count
+			print '------------unique_urls----------------'
+			print unique_urls[url_count][0]
+			print unique_urls
+			print '------------url_count----------------'
+			print url_count
+			if unique_urls[url_count][0] == ipfield_next:
+				#print '---------------------------------YYYYYYYYYYYYYYYYYSSSSSSSSSSSSSSSSS ipfield'
+				#User Agent
+				user_agent1 = httpagentparser.simple_detect(unique_urls[url_count][4])
+				user_agent2 = httpagentparser.simple_detect(k[4])
+				if user_agent1[0] == user_agent2[0] and user_agent1[1] == user_agent2[1]:
+					print '---------------------------------YYYYYYYYYYYYYYYYYSSSSSSSSSSSSSSSSS user_agent'
+					print"----------user_agent1-------------"
+					print user_agent1
+					print"---------user_agent2------------"
+					print user_agent2
+					print '------------url_count----------------'
+					print url_count
+					# Find the time between the two requests
+					first_time = re.search(r'\d+/\w+/\d+:(\d+:\d+:\d+)\s', unique_urls[url_count][1])
+					print '---------first_time---------'
+					print first_time.group(1)
+					second_time = re.search(r'\d+/\w+/\d+:(\d+:\d+:\d+)\s', k[1])
+					print '---------second_time---------'
+					print second_time.group(1)						
+					FMT = '%H:%M:%S'
+					tdelta = datetime.strptime(second_time.group(1), FMT) - datetime.strptime(first_time.group(1), FMT)
+					diff_in_time = getSec(str(tdelta))
+					print '-----------------time difference------------'
+					print diff_in_time
+					print '------------url_count----------------'
+					print url_count
+
+					# If time between two requests are greater than the input time, consider it a unique visit
+					if diff_in_time > N:
+						print '---------------------------------YYYYYYYYYYYYYYYYYSSSSSSSSSSSSSSSSS difference'
+						print '------------unique_urls----------------'
+						unique_urls.append(k)
+						print unique_urls
+						print '------------url_count----------------'
+						url_count = url_count + 1
+						print url_count
+					else:
+						print"---------unique_urls--------------"
+						print unique_urls
+						continue
+			else:
+				unique_urls.append(k)
+				print"---------unique_urls--------------"
 				print unique_urls
-				if unique_urls[url_count][0] == ipfield_next:
-					#print '---------------------------------YYYYYYYYYYYYYYYYYSSSSSSSSSSSSSSSSS ipfield'
-					#User Agent
-					user_agent1 = httpagentparser.simple_detect(unique_urls[url_count][4])
-					user_agent2 = httpagentparser.simple_detect(count_list[boundvar][4])
-					if user_agent1[0] == user_agent2[0] and user_agent1[1] == user_agent2[1]:
-						print '---------------------------------YYYYYYYYYYYYYYYYYSSSSSSSSSSSSSSSSS user_agent'
-						#print '------------'
-						#print user_agent1[0]
-						#print user_agent1[1]
-						#print '------------'
-						#print user_agent2[0]
-						#print user_agent2[1]
-						print"----------user_agent1-------------"
-						print user_agent1
-						print"---------user_agent2------------"
-						print user_agent2
-						# Find the time between the two requests
-						first_time = re.search(r'\d+/\w+/\d+:(\d+:\d+:\d+)\s', unique_urls[url_count][1])
-						print '---------first_time---------'
-						print first_time.group(1)
-						second_time = re.search(r'\d+/\w+/\d+:(\d+:\d+:\d+)\s', count_list[boundvar][1])
-						print '---------second_time---------'
-						print second_time.group(1)						
-						FMT = '%H:%M:%S'
-						tdelta = datetime.strptime(second_time.group(1), FMT) - datetime.strptime(first_time.group(1), FMT)
-						diff_in_time = getSec(str(tdelta))
-						print '-----------------time difference------------'
-						print diff_in_time
-						# If time between two requests are greater than the input time, consider it a unique visit
-						if diff_in_time > N:
-							print '---------------------------------YYYYYYYYYYYYYYYYYSSSSSSSSSSSSSSSSS difference'
-							print '------------unique_urls----------------'
-							unique_urls.append(count_list[boundvar])
-							print unique_urls
-							print '------------url_count----------------'
-							url_count = url_count + 1
-							print url_count
-							print '------------boundvar----------------'
-							boundvar = boundvar + 1
-							print unique_urls
-							#if boundvar == size_list - 1:
-						#		check = 1
-						#		print 'yesssssss'
-			#			#	print 'nooooooo'
-						else:
-							boundvar = boundvar + 1
-				else:
-					unique_urls.append(count_list[boundvar])
-					url_count = url_count + 1
-					boundvar = boundvar + 1
-					#if boundvar == size_list - 1:
-						#check = 1
-			#		print 'yesssssss'
-					
-			#boundvar = boundvar + 1
-			#print '-----------unique_urls-----------'
-			#print unique_urls
-			#if check == 1:
-				#print 'cheeeeeeeeeeeeekkkkkkk'
-			#	unique_urls.append(k)
-			#	check = 0
-			#else:
-			#	continue
-			#url_count = url_count + 1
+				url_count = url_count + 1
+				print"---------unique_count--------------"
+				print url_count
+
+		print"---------unique_urls--------------"
+		print unique_urls
+		print unique_urls[0][3] + ' : ' + str(len(unique_urls))
+		unique_urls = []
 		print"---------unique_urls--------------"
 		print unique_urls
 		break
-	#print'2222222222222'
-	##print count_list[0]
-	#print count_list[0][1]
-	#print count_list[len(count_list)-2:len(count_list)]
-	#print count_list
-	#return
-		print '-------------------'
-	#print time_filter			
-	#s1 = time_filter[0][0]
-	#print s1
-	#s2 = time_filter[1][0]
-	#print s2
-	#FMT = '%H:%M:%S'
-	#tdelta = datetime.strptime(s2, FMT) - datetime.strptime(s1, FMT)
-	#print getSec(str(tdelta))
 
 # Calculate the seconds
 def getSec(s):
